aa08b187f1f0b33a60a6a567ec05922a9d700e2b
BurairAbbas <65166629+BurairAbbas@users.noreply.github.com>
2020-11-30 22:28:34 +0500

    Upload

---
+++ 22_LambdaExpression/Chap 22 Lambda Ex-Excercise.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Chap22LambaExpression
+{
+    class Excercise
+    {
+        public static void MainEx()
+        {
+            //// Part 1: Extension of StringBuilder SubString  which work like string substring
+            // StringBuilder str = new StringBuilder();
+            // str.Append("Hello");
+            // str.Append("World");
+            // StringBuilder str1 = str.SubString(0, 5);
+            // Console.WriteLine(str1);
+
+            // //Part 2: IEnumerable Extension of Sum, Max, Min, Avg
+            // List<int> numbers = new List<int> { 11, 2, 4, 10, 5, 6, 1 };
+            // int sum = numbers.Sum();
+            // int min = numbers.Min();
+            // int max = numbers.Max();
+            // double avg = numbers.Average();
+            // Console.WriteLine("Sum: {0} \tMinimun value: {1} \tMaximum Value: {2} \t Average: {3: 0.00}", sum, min, max, avg);
+
+            List<Student> students = new List<Student>()
+            {
+                new Student { FirstName = "John", LastName = "Barbosa", Age = 20},
+                new Student { FirstName = "Neil", LastName = "Rehna", Age = 20 },
+                new Student { FirstName = "Soha", LastName = "Arif", Age = 20 },
+                new Student { FirstName = "Ali", LastName = "Ahmed", Age = 19 },
+                new Student { FirstName = "Ron", LastName = "Uzham", Age = 17 },
+                new Student { FirstName = "Harry", LastName = "Alam", Age = 24},
+                new Student { FirstName = "Arian", LastName = "Alba", Age = 25 }
+            };
+
+            //var studentNameList = from student in students
+            //                      group student by student.LastName.CompareTo(student.FirstName) into groups
+            //                      select new { Key = groups.Key, Names = groups };
+
+            //foreach (var item in studentNameList)
+            //{
+            //    if (item.Key == -1)
+            //    {
+            //        foreach (var item2 in item.Names)
+            //        {
+            //            Console.WriteLine(item2);
+            //        }
+            //    }
+            //}
+
+            //One solution Two differenct approach
+
+            //var studentNameList = from student in students
+            //                      where student.LastName.CompareTo(student.FirstName) == -1
+            //                      select student;
+            //foreach (var item in studentNameList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine();
+
+            //// part 4: age difference between 18 to 24
+            //var studentWithAge = from student in students
+            //                     where (student.Age >= 18 && student.Age <= 24)
+            //                     select student;
+            //foreach (var item in studentWithAge)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //part 5: OrderBy and ThenBy in descending Order
+            //var descendingByFirstName = students.OrderBy();
+            //foreach (var item in descendingByFirstName)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var descendingByLastName = students.ThenBy();
+            //foreach (var item in descendingByLastName)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //// Part 6: use built-in method to find number multiple by 3 and 7
+            //List<int> numbers = new List<int> { 10, 5, 2, 6, 88, 49, 9, 111, 35, 3, 7, 13, 21 };
+            ////List<int> numberDivisibleBy3and7 = numbers.FindAll(x => (x % 3 == 0) && ( x % 7 == 0 ));
+
+            //var numberDivisibleBy3And7 = from number in numbers
+            //                                   where number % 3 == 0 && number % 7 == 0
+            //                                   select number;
+
+            //foreach (var item in numberDivisibleBy3And7)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            string sentence = "this iS a Sample sentence.";
+            string standardSentence = sentence.FirstLetterCapital();
+            Console.WriteLine(standardSentence);
+
+            PhoneBookDifferenceBetweenHashTableAndCollectionOfData();
+
+        }
+
+        public static void PhoneBookDifferenceBetweenHashTableAndCollectionOfData()
+        {
+            Dictionary<string, int> phoneBook = new Dictionary<string, int>();
+            string[] firstName = { 
+                "John", "Babosa", "Nail", "Arif", "Ron", "Rehman", "Ali", "Haider", "Kate", "Wilson", "Steve", "Milton", "John", "Peterson",
+                            "Curie", "Albert", "Tesla", "Bill", "Sam", "Iqbal", "Nazish", "Raza", "Rapshon", "Watson", "Dinael", "Brien",
+                            "Diease", "Washington", "Jocab", "Philip", "Max", "Bohr", "Newton", "Isaac", "Stovic", "Linton"};
+            string[] lastName = {
+               "John", "Babrosa", "Nail", "Arif", "Ron", "Rehman", "Ali", "Haider", "Kate", "Wilson", "Steve", "Milton", "John", "Peterson",
+                            "Curie", "Albert", "Tesla", "Bill", "Sam", "Iqbal", "Nazish", "Raza", "Rapshon", "Watson", "Dinael", "Brien",
+                            "Diease", "Washington", "Jocab", "Philip", "Max", "Bohr", "Newton", "Isaac", "Stovic", "Linton"};
+
+            int[] randomNumberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 10, 12, 45, 56, 76, 90, 43, 56, 72, 90 };
+            
+            Random rnd = new Random();
+            for (int i = 0; i < 50000; i++)
+            {
+                List<int> numbers = new List<int>();
+                string number = randomNumberArray[rnd.Next(0, 19)].ToString() + rnd.Next(10, 20).ToString() + rnd.Next(30, 40).ToString() +
+                    rnd.Next(40, 50).ToString();
+                string name = firstName[rnd.Next(0, firstName.Length)] + " " + lastName[rnd.Next(0, lastName.Length)];
+                if(!phoneBook.ContainsKey(name)) // to avoid collision
+                     phoneBook.Add(name, Convert.ToInt32(number));
+
+            }
+
+            DateTime startTime = DateTime.Now;
+            for (int i = 0; i < 50000; i++)
+            {
+                bool found = phoneBook.ContainsKey("Max Isaac");
+            }
+
+            Console.WriteLine("Contain method of table execute time: {0} ", DateTime.Now - startTime);
+            // outPut: 00.00.00
+
+            startTime = DateTime.Now;
+            for (int i = 0; i < 50000; i++)
+            {
+                phoneBook.Where(x => x.Key.Equals("Max Isaac"));
+            }
+            Console.WriteLine("Where method Execute time: {0}", DateTime.Now - startTime);
+            // output: 00.018
+        }
+
+
+    }
+    //Part 1:
+    public static class StringBuilderExtension
+    {
+        public static StringBuilder SubString(this StringBuilder str, int index, int length)
+        {
+            StringBuilder subString = new StringBuilder();
+            for (int i = index; i < length; i++)
+            {
+                subString.Append(str[i]);
+            }
+            return subString;
+        }
+    }
+
+    //Part 2:
+    public static class IEnumerableExtension
+    {
+        public static int Sum(this IEnumerable<int> element)
+        {
+            int sum = 0;
+            foreach (var item in element)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public static int Min(this IEnumerable<int> element)
+        {
+            int min = int.MaxValue;
+            foreach (var item in element)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+
+        public static int Max(this IEnumerable<int> element)
+        {
+            int max = 0;
+            foreach (var item in element)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        public static double Average(this IEnumerable<int> element)
+        {
+            double sum = 0;
+            foreach (var item in element)
+            {
+                sum += item;
+            }
+            double avg = sum / element.Count();
+            return avg;
+        }
+    }
+
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Student()
+        {
+            firstName = null;
+            lastName = null;
+            age = 0;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("FirstName: {0} LastName: {1} Age: {2} ", FirstName, LastName, Age);
+        }
+    }
+
+    //part 5: OrderBy and ThenBy in descending Order
+    public static class IListExtension
+    {
+        public static dynamic OrderBy(this List<Student> elements)
+        {
+            var sortedByFirstName = elements.OrderByDescending(x => x.FirstName);
+
+            //var sortedByFirstName = from element in elements
+            //                        orderby element.FirstName descending
+            //                        select element;
+
+            return sortedByFirstName;
+        }
+
+        public static dynamic ThenBy(this List<Student> elements)
+        {
+            //var sortedByLastName = element.OrderByDescending(x => x.LastName);
+
+            var sortedByLastName = from element in elements
+                                   orderby element.LastName descending
+                                   select element;
+
+            return sortedByLastName;
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string FirstLetterCapital(this string str)
+        {
+            StringBuilder capital = new StringBuilder();
+            string[] splitString = str.Split(' ');
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                //StackOverFlow
+                string restOfLetter = "";
+                if (splitString[i].Length > 1)
+                {
+                    restOfLetter = splitString[i].Substring(1).ToLower();
+                }
+                char firstChar = char.ToUpper(splitString[i][0]);
+                splitString[i] = firstChar + splitString[i].Substring(1).ToLower() + " ";
+                capital.Append(splitString[i]);
+            }
+
+            // Book solution
+            string capital2 = new CultureInfo("en-US", false).TextInfo.ToTitleCase(str);
+
+            return capital.ToString();
+        }
+
+    }
+
+
+}
---
+++ 22_LambdaExpression/Program.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Chap22LambaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Excercise.MainEx();
+
+
+            //// Extension Method Example
+            //string helloString = "Hello, Extension Method";
+            //int wordCount = helloString.WordCount();
+            //Console.WriteLine(wordCount);
+
+            //List<int> number = new List<int> { 1 };
+            //Console.WriteLine(number.ToString<int>());
+            //number.IncreaseWith(5);
+            //Console.WriteLine(number.ToString<int>());
+
+            //// Anonymous type 
+            //// Compiler guess the types of properties by it own
+            //var myCar = new { Color = "Red", Brand = "BMW", Speed = 180 };
+            //Console.WriteLine("My {0} Color is {1}", myCar.Brand, myCar.Color);
+            //Console.WriteLine("It run with speed of {0}km/h", myCar.Speed);
+
+            //// Compiler also generate the redefine method
+            //Console.WriteLine(myCar.ToString());
+            //Console.WriteLine("The hash code is: {0}", myCar.GetHashCode().ToString());
+            //Console.WriteLine("Equals? {0}", myCar.Equals(new { Color = "Red", Brand = "BMW", Speed = 180 }));
+            //Console.WriteLine("Speed is Equal? {0} ", myCar.Speed.Equals(180));
+            //Console.WriteLine("Type name: {0}", myCar.GetType());
+
+            //// Anonymous type Array
+            //var arr = new[] 
+            //{
+            //   new { X = 3, Y = 5 },
+            //   new { X = 2, Y = 10 },
+            //   new { X = 5, Y = 12 }
+            //};
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
+
+            //Lamba Expression
+
+            //List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            //List<int> evenNumber = list.FindAll(x => (x % 2) == 0);
+            //foreach (var item in evenNumber)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            //List<Dog> dogs = new List<Dog>()
+            //{
+            //    new Dog { Name = "Rex", Age = 11 },
+            //    new Dog { Name = "Pit", Age = 2 },
+            //    new Dog { Name = "Su", Age = 4 }
+            //};
+
+            //var names = dogs.Select(x => x.Name);
+
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //// Lamba with Anonymous Expression
+
+            //var newDogsList = dogs.Select(
+            //    x => new { Age = x.Age, FirstLetter = x.Name[0] });
+            //foreach (var item in newDogsList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////Sorting with lamba Expression
+
+            //var sortedDogs = dogs.OrderByDescending(x => x.Age);
+            //foreach (var item in sortedDogs)
+            //{
+            //    Console.WriteLine("Dog Name: {0} with age {1}", item.Name, item.Age);
+            //}
+
+            //// Lambda With Body
+            //var evenNumber2 = list.FindAll(i =>
+            //{
+            //    Console.WriteLine("Number in list are: {0}", i);
+            //    return i % 2 == 0;
+            //});
+
+            //foreach (var item in evenNumber2)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            //Delegates
+            //// parameter less function and return 'true' whenever call
+            //Func<bool> boolFunct = () => true;
+            ////function with parameter and return bool if condition is satisfied 
+            //Func<int, bool> intFunc = (x) =>
+            //    {
+            //        return x < 10;
+            //    };  
+            //if (boolFunct() && intFunc(11))
+            //{
+            //    Console.WriteLine("5 < 10");
+            //}
+
+            //LINQ
+
+            //var cul = (from culture in CultureInfo.GetCultures(CultureTypes.AllCultures)
+            //           where culture.Name.StartsWith("b")
+            //           select culture);
+            //foreach (var item in cul)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //List<int> number = new List<int>(){
+            //    1, 2, 3, 10, 5, 8, 7, 4, 9, 6
+            //};
+
+            //var evenNumber = from num in number
+            //                 where num % 2 == 0
+            //                 select num;
+            //foreach (var item in evenNumber)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //// sort
+            //var sortedNumber = from num in number
+            //                   orderby num ascending
+            //                   select num;
+            //foreach (var item in sortedNumber)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            //string[] words = { "Cherry", "WaterMalon", "Applepie" };
+            //var sortedWord = from word in words
+            //                 orderby word.Length ascending
+            //                 select word;
+            //foreach (var item in sortedWord)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            // Grouping
+            //int[] numbers ={
+            //    1, 2, 3, 4, 5, 6, 10, 12, 13, 14, 15, 9, 8
+            //};
+            //int divisor = 5;
+
+            //var numberGroup = from number2 in numbers
+            //                  group number2 by number2 % divisor into groups
+            //                  select new { Remainder = groups.Key, Numbers = groups };
+            //// the condition after by if become key 
+
+            //foreach (var item in numberGroup)
+            //{
+            //    Console.WriteLine(
+            //        "Numbers with a remainder of {0} when divided by {1}: ", item.Remainder, divisor);
+            //    foreach (var number in item.Numbers)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+
+            //List<Category> categorys = new List<Category>()
+            //{
+            //    new Category { ID = 1, Name = "Fruit" },
+            //    new Category { ID = 2, Name ="Food" },
+            //    new Category { ID = 3, Name = "Shoe" },
+            //    new Category { ID = 4, Name = "Juice"}
+            //};
+
+            //List<Product> products = new List<Product>()
+            //{
+            //    new Product { CategoryID = 1, Name = "berry"},
+            //    new Product { CategoryID = 1, Name = "banana" },
+            //    new Product { CategoryID = 3, Name = "Sandal" },
+            //    new Product { CategoryID = 2, Name = "Fish"},
+            //    new Product { CategoryID = 3, Name = "Chicken Meat" },
+            //    new Product { CategoryID = 4, Name = "Alpple Juice" },
+            //    new Product { CategoryID = 4, Name = "Orange Juice" }
+            //};
+
+            //var productWithCategory = from product in products
+            //                          join category in categorys
+            //                          on product.CategoryID equals category.ID
+            //                          select new { Name = product.Name, Category = category.Name };
+            //foreach (var item in productWithCategory)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////Nested Queue
+            //var productWithCategory2 = from product in products
+            //                           select new
+            //                           {
+            //                               Name = product.Name,
+            //                               Category =
+            //                               (from category in categorys
+            //                                where category.ID == product.CategoryID
+            //                                select category.Name)
+            //                           };
+            //foreach (var item in productWithCategory2)
+            //{
+            //    Console.Write(item.Name);
+            //    foreach (var item2 in item.Category)
+            //    {
+            //        Console.WriteLine(item2);
+            //    }
+            //}
+
+            // LINQ Performance
+            //    List<int> l1 = new List<int>();
+            //    DateTime startTime = DateTime.Now;
+            //    l1.AddRange(Enumerable.Range(1, 50000000));
+            //    Console.WriteLine("Ext.method: \t{0}", DateTime.Now - startTime);
+
+            //    startTime = DateTime.Now;
+            //    List<int> l2 = new List<int>();
+            //    for (int i = 1; i < 50000000; i++)
+            //    {
+            //        l2.Add(i);
+            //    }
+
+            //    Console.WriteLine("For-Loop: \t{0}", DateTime.Now - startTime);
+
+            //    List<int> list = new List<int>();
+            //    list.AddRange(Enumerable.Range(1, 100000));
+            //    startTime = DateTime.Now;
+            //    for (int i = 0; i < 10000; i++)
+            //    {
+            //        var element = list.Where(x => x > 20000);
+            //    }           
+            //    Console.WriteLine("No Execution: \t{0}", DateTime.Now - startTime);
+
+            //    startTime = DateTime.Now;
+            //    for (int i = 0; i < 10000; i++)
+            //    {
+            //        var element = list.Where(x => x > 1000).First();
+            //    }
+            //    Console.WriteLine("Execution: \t{0}", DateTime.Now - startTime);
+
+            //    // LINQ preformance with collection classe
+            //    HashSet<Guid> set = new HashSet<Guid>();
+            //    for (int i = 0; i < 50000; i++)
+            //    {
+            //        set.Add(Guid.NewGuid());
+            //    }
+
+            //    Guid keyForSearching = new Guid();
+            //    startTime = DateTime.Now;
+            //    for (int i = 0; i < 50000; i++)
+            //    {
+            //        bool found = set.Contains(keyForSearching);
+            //    }
+            //    Console.WriteLine("Hashtag: \t{0}", DateTime.Now - startTime);
+
+            //    startTime = DateTime.Now;
+            //    for (int i = 0; i < 50000; i++)
+            //    {
+            //        // Use IEnumerable<Guid>.Contains(…) extension method
+            //        bool found = set.Contains<Guid>(keyForSearching);
+            //    }
+            //    Console.WriteLine("Contains: {0}", DateTime.Now - startTime);
+            //    startTime = DateTime.Now;
+            //    for (int i = 0; i < 50000; i++)
+            //    {
+            //        // Use IEnumerable<Guid>.Where(…) extension method
+            //        bool found = set.Where(g => g == keyForSearching).Count() > 0;
+            //    }
+            //    Console.WriteLine("Where: {0}", DateTime.Now - startTime);
+            //}
+
+            //public class Product  
+            //{
+            //    public string Name { get; set; }
+            //    public int CategoryID { get; set; }
+            //}
+            //public class Category
+            //{
+            //    public string Name { get; set; }
+            //    public int ID { get; set; }
+            //}
+        }
+    }
+    
+    class Dog
+    {
+        private string name;
+        private int age;
+
+        public Dog()
+        {
+            this.name = null;
+            this.age = 0;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+
+    //public static class IntExtension
+    //{
+    //    public static int WordCount(this string str)
+    //    {
+    //        return str.Split(new char[] { '.', ' ', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+    //    }
+    //}
+
+    //public static class IListExtension
+    //{
+    //    public static void IncreaseWith(this List<int> list, int amount)
+    //    {
+    //        for (int i = 0; i < list.Count; i++)
+    //        {
+    //            list[i] += amount;
+    //        }
+    //    }
+    //}
+
+    //public static class IEnumerableExtension
+    //{
+    //    public static string ToString<T>(this IEnumerable<T> enumerable)
+    //    {
+    //        StringBuilder result = new StringBuilder();
+    //        result.Append('[');
+    //        foreach (var item in enumerable)
+    //        {
+    //            result.Append(item);
+    //            result.Append(", ");
+    //        }
+    //        if (result.Length > 1)
+    //            result.Remove(result.Length - 2, 2);
+    //        result.Append("]");
+    //        return result.ToString();
+    //    }
+    //}
+}
